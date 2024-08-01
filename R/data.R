0f98957f9a3b96b269a75d15ac25408935a432d1
afilonczuk <57341106+afilonczuk@users.noreply.github.com>
2024-08-01 10:46:43 -0400

    Update data.R with BFI-2 data

--- R/data.R
+++ R/data.R
@@ -7,3 +7,12 @@
 #'
 #' @source \url{https://philchalmers.github.io/mirt/html/SAT12.html}
 "SAT12"
+#'
+#' Description of BFI2 data
+#'
+#' Data obtained from the Big Five Inventory-2 (BFI-2; Ober et al., 2021) data collected from adolescents ages 14 to 17 years old. The inventory contains 60 items on a 5-point Likert scale, where exactly 12 items load onto each of the five personality factors.  Data for 838 subjects was collected, with no missing responses.
+#'
+#' @references Ober, T. M., Cheng, Y., Jacobucci, R., & Whitney, B. M. (2021, January). Examining the factor structure of the Big Five Inventory-2 personality domains with an adolescent sample. \emph{Psychological Assessment, 33}(1), 14–28. \url{doi:10.1037/pas0000962}
+#'
+#' @source \url{https://osf.io/awvnd}
+"BFI2"
