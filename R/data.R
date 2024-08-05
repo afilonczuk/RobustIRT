17087a64351692d27a7a3dc5d3345883a9286bbd
afilonczuk <57341106+afilonczuk@users.noreply.github.com>
2024-08-05 12:21:28 -0400

    Update documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' SAT12
 #'
-#' Description of SAT12 data
+#' Dichotomously-scored data for a 12th Grade science test
 #'
 #' Data obtained from the TESTFACT (Woods et al., 2003) manual, with 32 response pattern scored items for a grade 12 science assessment test (SAT) measuring topics of chemistry, biology, and physics. The scoring key for these data is [1, 4, 5, 2, 3, 1, 2, 1, 3, 1, 2, 4, 2, 1, 5, 3, 4, 4, 1, 4, 3, 3, 4, 1, 3, 5, 1, 3, 1, 5, 4, 5], respectively. However, careful analysis using the nominal response model suggests that the scoring key for item 32 may be incorrect, and should be changed from 5 to 3.
 #'
@@ -12,11 +12,12 @@
 #'
 #' BFI2
 #'
-#' Description of BFI2 data
+#' Polytomous data from the Big Five Inventory-2
 #'
-#' Data obtained from the Big Five Inventory-2 (BFI-2; Ober et al., 2021) data collected from adolescents ages 14 to 17 years old. The inventory contains 60 items on a 5-point Likert scale, where exactly 12 items load onto each of the five personality factors.  Data for 838 subjects was collected, with no missing responses.
+#' Data obtained from the Big Five Inventory-2 (BFI-2; Soto & John, 2017), collected from adolescents ages 14 to 17 years old enrolled in a high school AP Statistics course (Ober et al., 2021). The inventory contains 60 items on a 5-point Likert scale, where exactly 12 items load onto each of the five personality factors in the BFI-2 (Extraversion, Agreeableness, Conscientiousness, Negative Emotionality, and Open-Mindedness).  Data for 838 subjects was collected, after removing four subjects with missing data. No missing data remains. Variables are labeled by the first letter of the personality factor (e.g., E = Extraversion, A = Agreeableness, etc.) and item number 1 through 12. Items marked with a "R" were negatively worded on the assessment and responses have been computationally reverse coded.
 #'
 #' @references Ober, T. M., Cheng, Y., Jacobucci, R., & Whitney, B. M. (2021, January). Examining the factor structure of the Big Five Inventory-2 personality domains with an adolescent sample. \emph{Psychological Assessment, 33}(1), 14–28. \url{doi:10.1037/pas0000962}
+#' @references Soto, C. J., & John, O. P. (2017). The next Big Five Inventory (BFI-2): Developing and assessing a hierarchical model with 15 facets to enhance bandwidth, fidelity, and predictive power. \emph{Journal of Personality and Social Psychology, 113}, 117–143. \url{http://dx.doi.org/10.1037/pspp0000096}
 #'
 #' @source \url{https://osf.io/awvnd}
 "BFI2"
